f0fc8a11323a5b20d1e7dbfca4d4143cdb512310
srinath <americakart@gmail.com>
2015-04-15 11:25:07 +0530

    able to move the square

--- simulations/netsim.py
+++ simulations/netsim.py
@@ -27,38 +27,46 @@ class NetSim(object):
     def paint(self):
         
         #########################
-        p1=(100,200)
-        p2=(200,300)
-        p3=(300,400)
+        self.p1=(100,200)
+        self.p2=(200,300)
+        self.p3=(300,400)
         ########################
         
-        self.draw_line(self.DISPLAYSURF, Color.RED, p1, p2, 4)
-        self.draw_line(self.DISPLAYSURF, Color.RED, p2, p3, 4)
+        self.draw_line(self.DISPLAYSURF, Color.RED, self.p1, self.p2, 4)
+        self.draw_line(self.DISPLAYSURF, Color.RED, self.p2, self.p3, 4)
         
         self.squareImg = pygame.image.load('square1.png')
-        self.draw_circle(surf=self.DISPLAYSURF, color=Color.BLUE, center_point=p1, radius = 10)
-        self.draw_circle(surf=self.DISPLAYSURF, color=Color.BLUE, center_point=p2, radius = 10)
-        self.draw_circle(surf=self.DISPLAYSURF, color=Color.BLUE, center_point=p3, radius = 10)
+        self.draw_circle(surf=self.DISPLAYSURF, color=Color.BLUE, center_point=self.p1, radius = 10)
+        self.draw_circle(surf=self.DISPLAYSURF, color=Color.BLUE, center_point=self.p2, radius = 10)
+        self.draw_circle(surf=self.DISPLAYSURF, color=Color.BLUE, center_point=self.p3, radius = 10)
     
     
-    def get_position(self, p1, p2):
+    def get_position(self, p1, p2, increment=10):
         p1_x,p1_y=p1
         p2_x,p2_y=p2
-        
+        dx=(p2_x-p1_x)*1.0/increment
+        dy=(p2_y-p1_y)*1.0/increment        
+        dx,dy=2,2
+        return dx,dy
     def simulate(self):
         pygame.init()
         
         self.DISPLAYSURF = pygame.display.set_mode((1200, 600))
         pygame.display.set_caption("Grade Diffusion")
+        self.paint()
         
-        x=10
-        y=20
+        x=self.p1[0]
+        y=self.p1[1]
         while True:
             self.DISPLAYSURF.fill(Color.WHITE)
             self.paint()
-            self.DISPLAYSURF.blit(self.squareImg, (x, y))
-            x+=2
-            y+=2        
+            source_point = self.p1
+            end_point = self.p2
+            
+            self.DISPLAYSURF.blit(self.squareImg, (x,y))
+            dx,dy=self.get_position(source_point,end_point, )
+            x+=dx
+            y+=dy       
             #squareImg.scroll(x, y)
             for event in pygame.event.get():
                 if event.type == QUIT:
